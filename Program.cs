e84db5063ffe7419dc003f604a91cc2956e79982
Francis Grignon <live.com@ncis.ca>
2020-04-26 19:56:16 -0400

    Aventure de base

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace aventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom, reponse;
+
+            Afficher($"Ma première aventure");
+            LigneBlanche();
+
+            do {
+                nom = QuestionOuverte($"Quel est votre nom?");
+                reponse = QuestionOuiNon($"Est-ce que {nom} est bien votre nom?");
+            } while (reponse != "oui");
+
+            Afficher($"{nom} félicitation, votre aventure se terminer ici!");
+            LigneBlanche();
+
+            Afficher("Appuyer sur une touche pour quitter.");
+            Console.ReadKey();
+        }
+
+        ///////////////////////////////////////////////////////
+        // Fonctions d'aide
+        ///////////////////////////////////////////////////////
+
+        static void Afficher(string message) 
+        {
+            Console.WriteLine(message);
+        }
+
+        static void LigneBlanche() 
+        {
+            Console.WriteLine();
+        }
+
+        static string QuestionOuverte(string question) 
+        {
+            string reponse;
+
+            Console.WriteLine($"{question}");
+            
+            reponse = Console.ReadLine();
+
+            return reponse;
+        }
+
+        static string QuestionOuiNon(string question) 
+        {
+            string reponse;
+
+            do 
+            {
+                Console.WriteLine($"{question} (oui/non)");
+                
+                reponse = Console.ReadLine();
+            } while (reponse != "oui" && reponse != "non");
+
+            return reponse;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# aventure
-Basic text aventure
+# Aventure
+Petit programme pour créer une aventure en texte.
+
+# Preérequis
+dotnet core 3.1 sdk
+https://dotnet.microsoft.com/download/dotnet-core/3.1
+
+git
+https://git-scm.com/download/win
+
+# Comment compiler?
+
+Ouvrez un l'invite de commande et tapez:
+
+``` 
+git clone https://github.com/FrancisGrignon/aventure.git`
+cd aventure
+dotnet build
+```
+
+# Comment executer?
+
+Simplement utiliser la commande suivante:
+
+`dotnet run`
+
+# Comment partager?
+
+`dotnet publish -r win-x64 --self-contained=false /p:PublishSingleFile=true -p:PublishDir=.\publish -c release`
+
+Votre programme se trouve dans le dossier "publish". Vous pouvez garder seulement le fichier aventure.exe. Le second fichier sert à débugger l'application.
+
+# Comment modifier l'aventure?
+
+Ouvrir le fichier `Program.cs` avec votre éditeur de texte préféré et modifier votre aventure.
